87638d560a8d151ad90b6a361a01f5e473e1742b
BhaveshDeveloper125 <bhavesh2103m@gmail.com>
2024-06-20 16:08:26 +0530

    code updated

--- Video-streaming app/src/Component/MostViewed/Mostviewed.jsx
+++ Video-streaming app/src/Component/MostViewed/Mostviewed.jsx
@@ -15,65 +15,159 @@ function Mostviewed()
         <>
             <div className="anotherbox">
                 <table>
+                    <tr className='viewedtitle'>
+                        <td>
+                            <br />
+                            <h1>
+                                Most Viewed Category
+                            </h1>
+                            <br />
+                        </td>
+                    </tr>
                     <tr>
                         <td>
-                            <Card url={deku}/>
+                            <Card url={deku} title={'My Hero Academia'}/>
                         </td>
                         <td>
-                            <h1>
-                                MostViewed Title
+                            <h1 className='maintitle'>
+                                My Hero Academia
                             </h1>
                         </td>
                     </tr>
                     <br />
+                    
                     <tr>
                         <td>
-                            <Card url={goku}/>
+                            <Card url={goku} title={'Bragon Ball Z'}/>
                         </td>
                         <td>
-                            <h1>
-                                MostViewed Title
+                            <h1 className='maintitle'>
+                                Dragon Ball Z
                             </h1>
                         </td>
                     </tr>
                     <br />
+                    
                     <tr>
                         <td>
-                            <Card url={aot}/>
+                            <Card url={aot} title={'Attack On Titan'}/>
                         </td>
                         <td>
-                            <h1>
-                                MostViewed Title
+                            <h1 className='maintitle'>
+                                Attack on Titan
                             </h1>
                         </td>
                     </tr>
                     <br />
+                    
                     <tr>
                         <td>
-                            <Card url={ms}/>
+                            <Card url={ms} title={'My Star'}/>
                         </td>
                         <td>
-                            <h1>
-                                MostViewed Title
+                            <h1 className='maintitle'>
+                                My Star
                             </h1>
                         </td>
                     </tr>
                     <br />
+                    
                     <tr>
                         <td>
-                            <Card url={gokuu}/>
+                            <Card url={gokuu} title={'Dragon Ball Z'}/>
                         </td>
                         <td>
+                            <h1 className='maintitle'>
+                                Dragon Ball Z
+                            </h1>
+                        </td>
+                    </tr>
+                    <br /><br />
+
+                    <tr>
+                        <td>
+                            <h2>        
+                                View More
+                            </h2>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+            <div className="anotherbox2">
+                <table>
+                    <tr className='populartitle'>
+                        <td>
+                            <br />
                             <h1>
-                                MostViewed Title
+                                Most Popular Category
+                            </h1>
+                            <br />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Card url={deku} title={'My Hero Academia'}/>
+                        </td>
+                        <td>
+                            <h1 className='maintitle'>
+                                My Hero Academia
                             </h1>
                         </td>
                     </tr>
                     <br />
+                    
                     <tr>
                         <td>
-                            <h2>
-                                <br /><br />
+                            <Card url={goku} title={'Bragon Ball Z'}/>
+                        </td>
+                        <td>
+                            <h1 className='maintitle'>
+                                Dragon Ball Z
+                            </h1>
+                        </td>
+                    </tr>
+                    <br />
+                    
+                    <tr>
+                        <td>
+                            <Card url={aot} title={'Attack On Titan'}/>
+                        </td>
+                        <td>
+                            <h1 className='maintitle'>
+                                Attack on Titan
+                            </h1>
+                        </td>
+                    </tr>
+                    <br />
+                    
+                    <tr>
+                        <td>
+                            <Card url={ms} title={'My Star'}/>
+                        </td>
+                        <td>
+                            <h1 className='maintitle'>
+                                My Star
+                            </h1>
+                        </td>
+                    </tr>
+                    <br />
+                    
+                    <tr>
+                        <td>
+                            <Card url={gokuu} title={'Dragon Ball Z'}/>
+                        </td>
+                        <td>
+                            <h1 className='maintitle'>
+                                Dragon Ball Z
+                            </h1>
+                        </td>
+                    </tr>
+                    <br /><br />
+
+                    <tr>
+                        <td>
+                            <h2>        
                                 View More
                             </h2>
                         </td>
