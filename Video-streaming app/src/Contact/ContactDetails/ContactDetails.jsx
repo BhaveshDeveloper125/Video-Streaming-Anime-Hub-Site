0dfc5056ca036cc74fa7d11fd3e2604873f1d448
BhaveshDeveloper125 <bhavesh2103m@gmail.com>
2024-06-21 23:33:39 +0530

    code update

---
+++ Video-streaming app/src/Contact/ContactDetails/ContactDetails.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './ContactUs.css'
+
+
+function ContactDetails() 
+{
+    return(
+        <>
+            <h1>
+                Conatact US
+            </h1>
+            <h3>
+                Please submit your inquiry using the form below and we will get in touch with you shortly.
+            </h3>
+        </>
+    );
+}
+
+export default ContactDetails
+
