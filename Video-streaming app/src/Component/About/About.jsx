242c771fe3bb3044129415d21f0dca065497f995
BhaveshDeveloper125 <bhavesh2103m@gmail.com>
2024-07-01 18:02:05 +0530

    1st july 6:10pm

--- Video-streaming app/src/Component/About/About.jsx
+++ Video-streaming app/src/Component/About/About.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import './About.css'
 import crunchy from '../../assets/crunchy.jpg'
+import event from '../../assets/events.jpg'
+import merch from '../../assets/merch.jpg'
+import streaming from '../../assets/streaming.jpg'
+import theater from '../../assets/theater.jpg'
+import comicbook from '../../assets/comicbook.jpg'
+import mobilegames from '../../assets/mobilegames.jpg'
+import Footer from '../Footer/Footer'
+import { Link } from 'react-router-dom';
+
+
 
 function About() 
 {
@@ -20,10 +30,85 @@ function About()
 
             </div>
 
+            <br /><br />
+
             <button className='hiring'>
                 We're Hiring
             </button>
-        </div>
+
+            <br /><br /><br /><br /><br />
+
+            <div className="activity_container1">
+                <img src={streaming} alt=""  className='marging_img'/>
+
+                
+                <img src={event} alt=""  className='marging_img'/>
+            
+            </div>
+
+                <h2 className='straming_events_text' id='stream_text'>
+                    Streaming
+                </h2>
+
+                <h2 className='straming_events_text' id='event_text'>
+                    Events
+                </h2>
+
+
+            <br /><br /><br />
+
+            <div className="activity_container2">
+
+                <img src={theater} alt=""  className='marging_img'/>
+                
+                <img src={merch} alt=""  className='marging_img'/>
+
+            </div>
+
+            <h2 className='straming_events_text' id='theater_text'>
+
+                THEATRICAL  DISTRIBUTION
+                    
+            </h2>
+
+            <h2 className='straming_events_text' id='merch_text'>
+                MERCHANDISE   
+            </h2>
+
+            <br /><br /><br />
+
+
+            <div className="activity_container3">
+
+                <img src={mobilegames} alt=""  className='marging_img'/>
+                
+                <img src={comicbook} alt=""  className='marging_img'/>
+
+            </div>
+
+            <h2 className='straming_events_text' id='mobilegame_text'>
+
+                MOBILE GAMES
+                    
+            </h2>
+
+            <h2 className='straming_events_text' id='manga_text'>
+                MANGA/COMIC BOOKS   
+            </h2>
+
+            <br /><br /><br /><br /><br />
+
+                <button className='hiring'>
+                    <Link to="/" id='link_color'>
+                        Home
+                    </Link>
+                </button>
+
+                <br /><br /><br /><br /><br />
+
+
+            
+        </div> 
     );
 }
 
